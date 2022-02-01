a224105295590d0506a2f3a5a457a6bb1e889269
Hornyák Richárd <hornyak.richard94@stud.uni-obuda.hu>
2022-02-01 15:09:18 +0100

    Fixing import

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcryptjs/dist/bcrypt";
+import bcrypt from "bcryptjs/dist/bcrypt.js";
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import data from "../data.js";
